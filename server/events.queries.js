dbb00a509193a78dbdb501ee52d859ce446ab60e
Jon Lazarowicz <jonlazar12@gmail.com>
2017-08-21 15:34:43 -0400

    updating events to async syntax

--- server/events.queries.js
+++ server/events.queries.js
@@ -1,92 +1,90 @@
-var promise = require('bluebird');
+const promise = require('bluebird');
 
-var options = {
+const options = {
   // Initialization Options
   promiseLib: promise
 };
 
-var pgp = require('pg-promise')(options);
-var connectionString = 'postgres://localhost:5432/acsc_db';
-var db = pgp(connectionString);
+const pgp = require('pg-promise')(options);
+const connectionString = 'postgres://localhost:5432/acsc_db';
+const db = pgp(connectionString);
 
 
 // Event Functions *********************
-function getAllEvents(req, res, next) {
-  db.any('select * from events')
-    .then(function (data) {
-      res.status(200)
-        .json({
-          status: 'success',
-          data: data,
-          message: 'Retrieved ALL events'
-        });
-    })
-    .catch(function (err) {
-      return next(err);
+async function getAllEvents(req, res, next) {
+  let data;
+  try {
+    data = await db.any('select * from events');
+  } catch (err) {
+    return next(err);
+  }
+  res.status(200)
+    .json({
+      status: 'success',
+      data: data,
+      message: 'Retrieved ALL events'
     });
 }
 
-function getSingleEvent(req, res, next) {
-  var id = parseInt(req.params.id);
-  db.one('select * from events where id = $1', id)
-    .then(function (data) {
-      res.status(200)
-        .json({
-          status: 'success',
-          data: data,
-          message: 'Retrieved ONE event'
-        });
-    })
-    .catch(function (err) {
-      return next(err);
+async function getSingleEvent(req, res, next) {
+  let id = parseInt(req.params.id),
+    data;
+  try {
+    data = await db.one('select * from events where id = $1', id);
+  } catch (err) {
+    return next(err);
+  }
+  res.status(200)
+    .json({
+      status: 'success',
+      data: data,
+      message: 'Retrieved ONE event'
     });
 }
 
-function createEvent(req, res, next) {
-  console.log(req.body)
-  // req.body.age = parseInt(req.body.age);
-  db.none('insert into events(location, event_date)' +
+async function createEvent(req, res, next) {
+  try {
+    await db.none('insert into events(location, event_date)' +
       'values(${location}, ${event_date})',
-    req.body)
-    .then(function () {
-      res.status(200)
-        .json({
-          status: 'success',
-          message: 'Inserted one event'
-        });
-    })
-    .catch(function (err) {
-      return next(err);
+      req.body)
+  } catch (err) {
+    return next(err);
+  }
+
+  res.status(200)
+    .json({
+      status: 'success',
+      message: 'Inserted one event'
     });
 }
 
-function updateEvent(req, res, next) {
-  db.none('update events set location=$1, event_date=$2 where id=$3',
-    [req.body.location, req.body.event_date, parseInt(req.params.id)])
-    .then(function () {
-      res.status(200)
-        .json({
-          status: 'success',
-          message: 'Updated event'
-        });
-    })
-    .catch(function (err) {
-      return next(err);
+async function updateEvent(req, res, next) {
+  try {
+    await db.none('update events set location=$1, event_date=$2 where id=$3',
+      [req.body.location, req.body.event_date, parseInt(req.params.id)])
+  } catch (err) {
+    return next(err);
+  }
+  res.status(200)
+    .json({
+      status: 'success',
+      message: 'Updated event'
     });
 }
 
-function removeEvent(req, res, next) {
-  var id = parseInt(req.params.id);
-  db.result('delete from events where id = $1', id)
-    .then(function (result) {
-      res.status(200)
-        .json({
-          status: 'success',
-          message: `Removed ${result.rowCount} event`
-        });
-    })
-    .catch(function (err) {
-      return next(err);
+async function removeEvent(req, res, next) {
+  let id = parseInt(req.params.id);
+  try {
+    await db.result('delete from events where id = $1', id)
+  }
+  catch (err) {
+    return next(err);
+  }
+
+  res.status(200)
+    .json({
+      status: 'success',
+      message: `Removed ${result.rowCount} event`
     });
 }
 
